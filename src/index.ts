e5fa78fa9e53758d3260f44c3911f5826fb3dd77
Joe Flateau <joe@joeflateau.net>
2022-02-24 19:31:33 -0500

    unique file names

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,42 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
-import { writeFile } from 'fs/promises';
+import { access, writeFile } from 'fs/promises';
 import keypair from 'keypair';
 
 async function main() {
   await new Command()
-    .argument('[name]', 'An alternate name for your keys', '')
+    .argument('[name]', 'An alternate name for your keys', 'key')
     .option('--bits <bits>', 'Key length', '2048')
     .action(async (name, options) => {
       const pair = keypair({ bits: Number(options.bits) });
-      const prefix = name ? `${name}.` : '';
-      await writeFile(`${prefix}public.pem`, pair.public);
-      await writeFile(`${prefix}private.pem`, pair.private);
+
+      let uniqueCounter = 0;
+      let publicKeyFilename: string;
+      let privateKeyFilename: string;
+
+      do {
+        uniqueCounter++;
+        const uniqifier = uniqueCounter === 1 ? '' : uniqueCounter;
+        publicKeyFilename = `${name}${uniqifier}.public.pem`;
+        privateKeyFilename = `${name}${uniqifier}.private.pem`;
+      } while (
+        (await exists(publicKeyFilename)) ||
+        (await exists(privateKeyFilename))
+      );
+
+      await writeFile(publicKeyFilename, pair.public);
+      await writeFile(privateKeyFilename, pair.private);
     })
     .parseAsync(process.argv);
+}
+
+async function exists(filename: string) {
+  try {
+    await access(filename);
+    return true;
+  } catch {
+    return false;
+  }
 }
 
 main();
